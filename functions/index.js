c075c3c7bdefbe441c7f5bf37920bf878ca53afc
GreenAvocado <greenavocado@protonmail.com>
2020-05-14 00:24:41 -0600

    feat: added firebase hosting, added function to serve test image

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,54 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const puppeteer = require('puppeteer');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(`
+function generateHTML(stateName, tables) {
+    return `
         <!DOCTYPE html>
         <html>
         <head>
+        <style>
+        .body {
+        font-family:arial;
+        }
+
+        td {
+        overflow:hidden;padding:2px 3px;vertical-align:bottom;border:1px solid rgb(204,204,204);
+        }
+
+        th {
+        overflow:hidden;padding:2px 3px;vertical-align:bottom;border:1px solid rgb(204,204,204);
+        }
+        </style>
+
         </head>
         <body>
-            Hello World
+        <div> <font size="5" style="font-size:small;font-family: Arial;">
+        <p>These are ` + stateName + `'s officially reported numbers for the 24-hour period ending at 5:30pm EST today:<p>
+        </font> </div>
+
+        ` + tables + `
+
+        <div> <font size="5" style="font-size:small;font-family: Arial;">
+        <p><i>Special thanks to The COVID Tracking Project and ` + stateName + ` state health authorities for supplying our data.</i><br>
         </body>
         </html>
-    `);
+    `;
+}
+
+app.get('/**', (req, res) => {
+    var image;
+    (async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setContent(generateHTML('a','b'))
+    image = await page.screenshot()
+    await browser.close()
+    })().then(function() {
+        res.contentType('image/jpeg');
+        res.send(image);
+    });
 });
 
 exports.app = functions.https.onRequest(app);
