78124a22c474de42190b0657a5f30caffa53d4f6
GreenAvocado <greenavocado@protonmail.com>
2020-05-14 23:16:12 -0600

    feat: optimised twitter card image, added dynamic page for twitter

--- functions/index.js
+++ functions/index.js
@@ -51,15 +51,10 @@ function generateHTML(stateName, tables) {
 
         </head>
         <body>
-        <div style="padding-left:44px;width:600px;">
-        <div> <font size="5" style="font-size:small;font-family: Arial;">
-        <p>These are ` + stateName + `'s officially reported numbers for the 24-hour period ending at 5:30pm EST today:<p>
-        </font> </div>
+        <div style="width:495px;height:267px">
 
         ` + tables + `
 
-        <div> <font size="5" style="font-size:small;font-family: Arial;">
-        <p><i>Special thanks to The COVID Tracking Project and ` + stateName + ` state health authorities for supplying our data.</i><br>
         </div>
         </body>
         </html>
@@ -82,17 +77,40 @@ app.post('/update/**', (req, res) => {
     (async () => {
     const browser = await puppeteer.launch({
         args: ['--no-sandbox'],
-        defaultViewport: {width: 688, height: 360}
+        defaultViewport: {width: 510, height: 267}
     });
     const page = await browser.newPage()
     await page.setContent(generateHTML(req.path.split('/')[2],req.body.table))
-    image = await page.screenshot({fullPage: true})
+    image = await page.screenshot()
     await browser.close()
     })().then(function() {
         saveImage(req.path.split('/')[2], image);
         res.contentType('image/png');
         res.status(200).send(generateHTML(req.path.split('/')[2],req.body.table));
     });
+});
+
+app.get('/**', (req, res) => {
+    console.log(req.path.split('/')[1])
+    res.status(200).send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <meta name="twitter:card" content="summary_large_image">
+            <meta name="twitter:title" content="` + req.path.split('/')[1] + `'s COVID-19 daily report via covidping.com">
+            <meta name="twitter:image" content="https://covidpingimages.web.app/image/` + req.path.split('/')[1] + `?` + Math.floor(Math.random() * 1000000000000000) + `">
+
+            <meta http-equiv="refresh" content="0; url=https://www.covidping.com" />
+        </head>
+        <body>
+            <p>Redirecting you to covidping.com</p>
+            <p>If you have not been redirected, <a href="https://www.covidping.com">click here.</a></p>
+        </body>
+        </html>
+    `);
 });
 
 const runtimeOpts = {
